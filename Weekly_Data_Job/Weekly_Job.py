78fbb7d42005f4e5419b4acbc392c76debf553dc
Erik Hall <erikhall6373@gmail.com>
2023-07-02 23:18:06 -0500

    Adjusting Absolute Path

--- Weekly_Data_Job/Weekly_Job.py
+++ Weekly_Data_Job/Weekly_Job.py
@@ -28,5 +28,5 @@ weekly_df['Power5'] = np.where(weekly_df['School'] == 'Notre Dame', 1, weekly_df
 weekly_df = weekly_df[analysis_cols]
 
 abs_path = os.path.abspath('.\Data')
-weekly_df.to_csv(abs_path + 'Weekly_Data.csv', index = False)
+weekly_df.to_csv(os.path.join(abs_path, 'Weekly_Data.csv'), index = False)
 
