e0b7b648014447de0e8841fee4a169e24e6e8145
Erik Hall <erikhall6373@gmail.com>
2023-07-25 16:14:00 -0500

    Rearranging File Structure

--- heisman_project/Weekly_Data_Job/Weekly_Job.py
+++ heisman_project/Weekly_Data_Job/Weekly_Job.py
@@ -3,12 +3,12 @@ import os
 import pandas as pd
 import numpy as np
 
-project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(project_path)
+#project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+#sys.path.append(project_path)
 
-from heisman_model.Data_Wrangling import Quarterback_Stats
-from heisman_model.Data_Wrangling import CPI_Ratings
-from heisman_model.Data_Cleaning import model_data_clean
+from heisman_project.heisman_model.Data_Wrangling import Quarterback_Stats
+from heisman_project.heisman_model.Data_Wrangling import CPI_Ratings
+from heisman_project.heisman_model.Data_Cleaning import model_data_clean
 
 os.remove('./Data/Weekly_Data.csv')
 
