493776afe7901a40de0d17d24dea9799f40f773b
Erik Hall <erikhall6373@gmail.com>
2024-09-03 15:57:59 -0500

    Small change to file paths in app

--- Shiny_App/app.py
+++ Shiny_App/app.py
@@ -8,11 +8,11 @@ import plotly.express as px
 from pathlib import Path
 import os
 
-app_path = os.path.dirname(os.path.abspath("__file__")) + "\Shiny_App"
-model_path =  app_path + "\heisman_model.pkl"
-model_data_path = app_path + "\Model_Data.csv"
-weekly_data_path = app_path  + "\Weekly_Data.csv"
-top_ten_html_path = app_path  + "\weekly.html"
+app_path = os.path.dirname(os.path.abspath("__file__")) + "/Shiny_App"
+model_path =  app_path + "/heisman_model.pkl"
+model_data_path = app_path + "/Model_Data.csv"
+weekly_data_path = app_path  + "/Weekly_Data.csv"
+top_ten_html_path = app_path  + "/weekly.html"
 
 
 model = pd.read_pickle(model_path)
